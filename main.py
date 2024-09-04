0a7b69b6fc57a647e87b9a80b32b80f32e17e8bb
jaedeok-lee1023 <jaedeok.lee_b@kurlycorp.com>
2024-09-04 14:22:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def send_slack_message(message, channel):
 def main():
     for cluster in clusters:
         # 메시지 제목 설정
-        header = f":메가폰: *『인사총무팀 공지』* f"@channel\n\n"
+        header = f":메가폰: *『인사총무팀 공지』* "@channel \n\n"
 
 
         notice_msg = (
